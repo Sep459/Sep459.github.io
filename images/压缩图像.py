4038cd1202a7c6b935c36c5030dc7b385a44cf0b
hky-Dorm <1830495015@qq.com>
2025-11-15 21:17:06 +0800

    Site updated: 2025-11-15 21:16:19

--- images/压缩图像.py
+++ images/压缩图像.py
@@ -8,7 +8,7 @@ img_dir = r"F:\Hexo\source\images\daily"
 img_dir = r"F:\Hexo\source\images\pet"
 img_dir = r"F:\Hexo\source\images\food"
 '''
-img_dir = r"F:\Hexo\source\images\gallery"
+img_dir = r"F:\Hexo\source\images\pet"
 
 max_side = 2000   # 最大边长
 quality = 85      # 压缩质量
